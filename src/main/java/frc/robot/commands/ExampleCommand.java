d3c9541cff4fbecbd2ff5797f54070e5987f3283
Tommy Tang <tjtang0310@gmail.com>
2023-01-13 15:07:40 -0500

    fdaf arf

---
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class ExampleCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final ExampleSubsystem m_subsystem;
+  private final Joystick m_joystick;
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public ExampleCommand(ExampleSubsystem subsystem, Joystick joystick) {
+    m_subsystem = subsystem;
+    m_joystick = joystick;
+    
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double move = m_joystick.getRawAxis(1);
+    double turn = m_joystick.getRawAxis(2);
+    m_subsystem.setEMotorSpeed(move+ turn);
+    m_subsystem.setWMotorSpeed(move- turn);
+    boolean mode = m_joystick.getRawButton(0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
